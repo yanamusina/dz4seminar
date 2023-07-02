14592f192f322e70957802217460964872a94502
яна <musina.yana02@yandex.ru>
2023-07-02 12:49:01 +0500

    commit

---
+++ task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SumNumber(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int number = ReadInt("введите число: ");
+System.Console.WriteLine($"{number} -> {SumNumber(number)}");
---
+++ task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]=rand.Next(-100,100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine(string.Join(" ", arr));
+}
+int[] array = new int [8];
+FillArray(array);
+PrintArray(array);
